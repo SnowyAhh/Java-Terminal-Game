803c62e7aa54484adcc51c6db20eebaa1b07f113
SnowyAhh <sylviahuynh17@gmail.com>
2025-04-20 12:47:50 +1000

    Added mathQuiz for the menu

--- Main.java
+++ Main.java
@@ -5,16 +5,24 @@ public class Main {
     public static void main(String args[]) {
         Menu menu = new Menu();
         Util util = new Util();
-        char[] validCharList = new char[1];
         String input = " ";
         Scanner scanner = new Scanner(System.in);
         rockPaperScissors rpsGame = new rockPaperScissors();
+        mathQuiz mathQuiz = new mathQuiz();
 
         while (input.charAt(0) != 'q') {
             menu.mainMenu();
             input = util.getValidInput(scanner, true, true, new char[]{'q'});
-            if (input.equals("1")) {
-                rpsGame.game();
+            switch (input) {
+                case "1":
+                    rpsGame.game();
+                    break;
+                case "2":
+                    // FIXME: add guess the number
+                    break;
+                case "3":
+                    mathQuiz.game();
+                    break;
             }
         }
         
