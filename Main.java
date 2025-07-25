a6d305a0a815d2f04ea8be86452e7e409a6801ff
SnowyAhh <sylviahuynh17@gmail.com>
2025-07-25 21:38:52 +1000

    Added input for guess the number selection

--- Main.java
+++ Main.java
@@ -5,16 +5,19 @@ public class Main {
     public static void main(String args[]) {
         Menu menu = new Menu();
         Util util = new Util();
-        char[] validCharList = new char[1];
         String input = " ";
         Scanner scanner = new Scanner(System.in);
         rockPaperScissors rpsGame = new rockPaperScissors();
+        guessTheNumber gtnGame = new guessTheNumber();
 
         while (input.charAt(0) != 'q') {
             menu.mainMenu();
             input = util.getValidInput(scanner, true, true, new char[]{'q'});
             if (input.equals("1")) {
                 rpsGame.game();
+            }
+            else if (input.equals("2")) {
+                gtnGame.game();
             }
         }
         
